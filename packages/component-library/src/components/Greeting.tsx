ff509ac05d35722d923f47fb2180ff8c057baa26
Josh Lalonde <mori.ryuuji3@gmail.com>
2025-09-27 11:07:35 -0300

    feat: create component-library package

---
+++ packages/component-library/src/components/Greeting.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react'
+
+function Greeting({ name }: GreetingProps): JSX.Element {
+    return <p>`Hello ${name}`</p>
+}
+
+interface GreetingProps {
+    name: string;
+}
+
+export default Greeting;
